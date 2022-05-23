b5e4244a56575cb473db1ddf0c1e0d040d330546
EdelDenis <edeldenis12@gmail.com>
2022-05-23 14:40:36 +0000

    l

--- script.js
+++ script.js
@@ -147,48 +147,50 @@ window.addEventListener("scroll",showModalByScroll);
 // Используем классы для карточек 
 
 class MenuCard {
-    constructor(src,alt,title,descr,price,parentSelector){
-        this.src=src;
-        this.alt=alt;
-        this.title=title;
-        this.descr=descr;
-        this.price=price;
+    constructor(src, alt, title, descr, price, parentSelector) {
+        this.src = src;
+        this.alt = alt;
+        this.title = title;
+        this.descr = descr;
+        this.price = price;
         this.parent = document.querySelector(parentSelector);
         this.transfer = 27;
-        this.changeToUAH();
-    }
-    changeToUAH(){
-        this.price= this.price * this.transfer;
+        this.changeToUAH(); 
     }
 
-    render(){
-        const element = document.createElement("div");
+    changeToUAH() {
+        this.price = this.price * this.transfer; 
+    }
+
+    render() {
+        const element = document.createElement('div');
         element.innerHTML = `
-        <div class="menu__item">
-                    <img src="img/tabs/vegy.jpg" alt="vegy">
-                    <h3 class="menu__item-subtitle">Меню "Фитнес"</h3>
-                    <div class="menu__item-descr">Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих
-                        овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной
-                        ценой и высоким качеством!</div>
-                    <div class="menu__item-divider"></div>
-                    <div class="menu__item-price">
-                        <div class="menu__item-cost">Цена:</div>
-                        <div class="menu__item-total"><span>229</span> грн/день</div>
-                    </div>
-                    `;
+            <div class="menu__item">
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            </div>
+        `;
         this.parent.append(element);
     }
 }
 
-const div = new MenuCard(
+new MenuCard(
     "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        9,
-        ".menu .container"
-);
-div.render();
+    "vegy",
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    9,
+    ".menu .container"
+).render();
+
+
+
 
 
 

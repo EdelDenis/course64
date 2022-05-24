2ea174a535153057928157dba3597bd8967731a3
EdelDenis <edeldenis12@gmail.com>
2022-05-24 12:35:54 +0000

    j

--- script.js
+++ script.js
@@ -147,49 +147,48 @@ window.addEventListener("scroll",showModalByScroll);
 // Используем классы для карточек 
 
 class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src,alt,title,descr,price,parentSelector){
         this.src = src;
         this.alt = alt;
         this.title = title;
         this.descr = descr;
         this.price = price;
-        this.parent = document.querySelector(parentSelector);
+        this.parent = document.querySelector("parentSelector");
         this.transfer = 27;
-        this.changeToUAH(); 
+        this.changeToUAH();
+    }
+    changeToUAH () {
+        this.price = this.price * this.transfer;
     }
 
-    changeToUAH() {
-        this.price = this.price * this.transfer; 
-    }
-
-    render() {
-        const element = document.createElement('div');
-        element.innerHTML = `
-            <div class="menu__item">
-                <img src=${this.src} alt=${this.alt}>
-                <h3 class="menu__item-subtitle">${this.title}</h3>
-                <div class="menu__item-descr">${this.descr}</div>
-                <div class="menu__item-divider"></div>
-                <div class="menu__item-price">
-                    <div class="menu__item-cost">Цена:</div>
-                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                </div>
-            </div>
+    render () {
+        const element = document.querySelector("div");
+        element.innerHTMl = `<div class="menu__item">
+        <img src=${this.src} alt=${this.alt}>
+        <h3 class="menu__item-subtitle">${this.title}</h3>
+        <div class="menu__item-descr">${this.descr}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+        </div>
+    </div>
         `;
         this.parent.append(element);
     }
 }
 
-new MenuCard(
+new MenuCard (
     "img/tabs/vegy.jpg",
     "vegy",
     'Меню "Фитнес"',
-    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    `Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих
+    овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной
+    ценой и высоким качеством!`,
     9,
-    ".menu .container"
+    ".menu.container"
+    
 ).render();
-
-
 
 
 
